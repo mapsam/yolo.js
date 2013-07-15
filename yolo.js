81a512334ecf3175f2be996f64cbdb995cdd9c4e
Chandler Sterling <chandlersterling1@gmail.com>
2013-07-15 14:48:31 -0700

    #yolo #swag #first #commit
    bare bones of yolo.js

---
+++ yolo.js
@@ -0,0 +1,21 @@
+var yoloCode;
+
+function yolo(e){
+	yoloCode = String.fromCharCode(e.charCode);
+	
+	console.log(yoloCode);
+	
+	if (yoloCode == "#"){
+		yoloEgg();
+	}	
+  }
+
+function yoloEgg(){
+	console.log('yolo fool');
+}
+ 
+ 
+	
+	
+
+
