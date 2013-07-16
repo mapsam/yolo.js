09f7378106bf1f309e0a740ed0e01a49cccd0503
Sam Matthews <matthews.sam@gmail.com>
2013-07-15 20:01:51 -0700

    yoloEgg() plays video

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ yolo.js
 =======
 
 You only live once. Egg your pages with funz.
+
+In the `<head>` of your document add the following:
+```html
+<script type="text/javascript" src="yolo.js"></script>
+```
+and attach the `onkeypress` event listener to the `<body>` of your document:
+```html
+<body onkeypress="yolo(event);">
+```
+
+Current List
+------------
+* yoloEgg() | **shift + 3** | The original. Plays [yolo](www.youtube.com/embed/z5Otla5157c) video within darkened div over entire screen.
+
+#yolo
+
--- yolo.js
+++ yolo.js
@@ -2,20 +2,32 @@ var yoloCode;
 
 function yolo(e){
 	yoloCode = String.fromCharCode(e.charCode);
-	
-	console.log(yoloCode);
-	
+	// use the log to test for charCode
+	// console.log(yoloCode);
+
+	// run functions based on keyboard charCodes
+	// input should be string and actual character, no ASCII or Integer business!
 	if (yoloCode == "#"){
 		yoloEgg();
 	}	
-  }
+}
 
 function yoloEgg(){
 	console.log('yolo fool');
+
+	// create holder for video
+	body = document.getElementsByTagName("body")[0];
+	full = document.createElement("div");
+	full.id = "fullScreen";
+	full.setAttribute("style", "position:absolute; top:0; left:0; height:100%; width:100%; text-align:center; background-color:rgba(0,0,0,0.8);");
+	body.appendChild(full);
+
+	// add video to new div
+	iframe = document.createElement("iframe");
+	iframe.setAttribute("width", "560");
+	iframe.setAttribute("height", "315");
+	iframe.setAttribute("style", "position:relative; top:50%; margin-top:-157.5px;");
+	iframe.setAttribute("src", "//www.youtube.com/embed/z5Otla5157c?autoplay=1");
+	iframe.setAttribute("frameborder", "0");
+	document.getElementById("fullScreen").appendChild(iframe);
 }
- 
- 
-	
-	
-
-
